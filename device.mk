483b38c66d619d7df6d40e4b7c881ad2398a5ac6
Sungwoo Park <swpark@nexell.co.kr>
2015-10-13 14:34:31 +0900

    [FIX] remove smartsync, add vendor/nexell/apps/smartsink.mk
    [FIX] remove old commented packages

--- device.mk
+++ device.mk
@@ -1,28 +1,4 @@
 ################################################################################
-# kernel
-################################################################################
-PRODUCT_COPY_FILES += \
-	kernel/arch/arm/boot/uImage:kernel
-
-################################################################################
-# bootloader
-################################################################################
-#PRODUCT_COPY_FILES += \
-	u-boot/u-boot.bin:bootloader
-
-################################################################################
-# 2ndboot
-################################################################################
-#PRODUCT_COPY_FILES += \
-	device/nexell/s5p6818_avn_ref/boot/2ndboot.bin:2ndbootloader
-
-################################################################################
-# overlay apps
-################################################################################
-#PRODUCT_COPY_FILES += \
-	#hardware/samsung_slsi/slsiap/overlay-apps/GooglePinyinIME.apk:system/app/PinyinIME.apk
-
-################################################################################
 # init
 ################################################################################
 PRODUCT_COPY_FILES += \
@@ -37,12 +13,6 @@ PRODUCT_COPY_FILES += \
 	device/nexell/s5p4418_msd8000b/factory_reset.sh:root/factory_reset.sh
 
 ################################################################################
-# bootanimation
-################################################################################
-#PRODUCT_COPY_FILES += \
-	device/nexell/s5p6818_avn_ref/bootanimation480x80.zip:system/media/bootanimation.zip
-
-################################################################################
 # key
 ################################################################################
 PRODUCT_COPY_FILES += \
@@ -54,26 +24,6 @@ PRODUCT_COPY_FILES += \
 ################################################################################
 PRODUCT_COPY_FILES += \
     device/nexell/s5p6818_avn_ref/tsc2007.idc:system/usr/idc/tsc2007.idc 
-
-
-
-################################################################################
-# hwc executable
-################################################################################
-PRODUCT_PACKAGES += \
-    report_hwc_scenario
-
-################################################################################
-# miracast sink
-################################################################################
-#PRODUCT_PACKAGES += \
-	#Mira4U
-
-################################################################################
-# storage
-################################################################################
-#PRODUCT_COPY_FILES += \
-	#device/nexell/s5p6818_avn_ref/vold.fstab:system/etc/vold.fstab
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -97,6 +47,7 @@ PRODUCT_COPY_FILES += \
 endif
 
 ################################################################################
+# media 
 ################################################################################
 PRODUCT_COPY_FILES += \
 	device/nexell/s5p6818_avn_ref/media_codecs.xml:system/etc/media_codecs.xml \
@@ -130,39 +81,13 @@ endif
 
 ################################################################################
 # wifi
-
+################################################################################
 PRODUCT_COPY_FILES += \
     hardware/samsung_slsi/slsiap/prebuilt/modules/wlan.ko:/system/lib/modules/wlan.ko
 
 ################################################################################
 # generic
 ################################################################################
-#PRODUCT_COPY_FILES += \
-  #device/nexell/s5p6818_avn_ref/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-  #frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-  #frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-  #frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-  #frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-  #frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-  #frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-  #linux/slsiap/library/lib/ratecontrol/libnxvidrc_android.so:system/lib/libnxvidrc_android.so
-
-#PRODUCT_COPY_FILES += \
-    #frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    #frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    #frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    #frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    #frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    #frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    #frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    #frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    #frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    #frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    #frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    #frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    #frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
-    #frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
-
 PRODUCT_COPY_FILES += \
 	device/nexell/s5p6818_avn_ref/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
@@ -177,7 +102,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	linux/platform/s5p6818/library/lib/libnxvidrc_android.so:system/lib/libnxvidrc_android.so
-
 
 PRODUCT_COPY_FILES += \
 	device/nexell/s5p6818_avn_ref/busybox-armv7l:system/bin/busybox-armv7l \
@@ -220,21 +144,16 @@ PRODUCT_PACKAGES += \
     e2fsck
 
 PRODUCT_PACKAGES += \
-                    TSCalibration   \
-                    libtslib    \
-                    inputraw    \
-                    pthres      \
-                    dejitter    \
-                    linear      \
-                    tscalib
-#
+    TSCalibration   \
+    libtslib    \
+    inputraw    \
+    pthres      \
+    dejitter    \
+    linear      \
+    tscalib
+
 PRODUCT_COPY_FILES += \
                     external/tslib/ts.conf:system/etc/ts.conf
-
-# Linaro
-#PRODUCT_PACKAGES += \
-	#GLMark2 \
-	#libglmark2-android
 
 # Product Property
 # common
@@ -243,23 +162,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 	wifi.supplicant_scan_interval=15 \
 	ro.sf.lcd_density=160
 
-# 4330 openl ui property
-#PRODUCT_PROPERTY_OVERRIDES += \
-	ro.opengles.version=131072 \	ro.hwui.texture_cache_size=72 \
-	ro.hwui.layer_cache_size=48 \
-	ro.hwui.path_cache_size=16 \
-	ro.hwui.shape_cache_size=4 \
-	ro.hwui.gradient_cache_size=1 \
-	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.texture_cache_flush_rate=0.4 \
-	ro.hwui.text_small_cache_width=1024 \
-	ro.hwui.text_small_cache_height=1024 \
-	ro.hwui.text_large_cache_width=2048 \
-	ro.hwui.text_large_cache_height=1024 \
-	ro.hwui.disable_scissor_opt=true
-
 # setup dalvik vm configs.
-#$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 
 # The OpenGL ES API level that is natively supported by this device.
@@ -273,24 +176,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	rtw_fwloader
 
-PRODUCT_PACKAGES += \
-	SmartSync
-
-# Enable AAC 5.1 output
-#PRODUCT_PROPERTY_OVERRIDES += \
-	media.aac_51_output_enabled=true
-
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
-
-# ota updater test
-#PRODUCT_PACKAGES += \
-	#OTAUpdateCenter
-
-# miracast sink
- #PRODUCT_PACKAGES += \
-	#Mira4U
+	persist.sys.usb.config=adb
 
 # wifi
 ifeq ($(BOARD_WIFI_VENDOR),realtek)
@@ -302,14 +190,9 @@ WIFI_BAND := 802_11_BG
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
 endif
 
-# 3G/LTE
-
 # call slsiap
 $(call inherit-product-if-exists, hardware/samsung_slsi/slsiap/slsiap.mk)
 
 # Nexell Application
 $(call inherit-product-if-exists, vendor/nexell/apps/nxapps.mk)
-
-# google gms
-#$(call inherit-product-if-exists, vendor/google/gapps/gapps.mk)
-# $(call inherit-product-if-exists, device/nexell/s5p6818_avn_ref/tiny_gapps.mk)
+$(call inherit-product-if-exists, vendor/nexell/apps/smartsync.mk)
