9d34035b7f2cace57cde7c81c07ed67ed8b63502
JongKeun Choi <jkchoi@nexell.co.kr>
2015-11-18 13:29:16 +0900

    [RST]restored backward remove after boot for avn_ref_s5p6818

--- start_deferred.sh
+++ start_deferred.sh
@@ -4,9 +4,6 @@ echo 1 > /proc/deferred_initcalls
 
 echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-# stop backward camera
-echo 1 > /sys/devices/platform/backward_camera/stop
-
 # printk log level
 echo 7 > /proc/sys/kernel/printk
 
