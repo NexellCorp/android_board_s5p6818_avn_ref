f97e1d4f976cbd60cc4bbeff349faaef6c6fa1fa
Sungwon Jo <doriya@nexell.co.kr>
2016-02-12 11:40:43 +0900

    [Media|Application] * Disable Default Application.

--- device.mk
+++ device.mk
@@ -252,5 +252,5 @@ endif
 $(call inherit-product-if-exists, hardware/samsung_slsi/slsiap/slsiap.mk)
 
 # Nexell Application
-$(call inherit-product-if-exists, vendor/nexell/apps/nxapps.mk)
+#$(call inherit-product-if-exists, vendor/nexell/apps/nxapps.mk)
 $(call inherit-product-if-exists, vendor/nexell/apps/smartsync.mk)
